ab38b31be97a14930f69dda4f13d5fdd23c68cf9
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-03 10:44:21 -0300

    s

--- js/principal.js
+++ js/principal.js
@@ -3,3 +3,6 @@ var titulo = document.querySelector("h1");
 console.log(titulo);
 console.log(titulo.textContent);
 titulo.textContent = ("Nego nutritations");
+
+var paciente = document.querySelector("primeiro-paciente");
+console.log(paciente)
