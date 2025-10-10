cbab0348f44668e30de55aa24d9b294ab385e7c3
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-10 09:42:42 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -24,3 +24,14 @@ var tdImc = document.querySelector(".info-imc");
 var imc = peso / (altura * altura);
 tdImc.textContent = imc;
 console.log(imc);
+
+if (peso < 0 || peso > 1000){
+    console.log("Peso inválido");
+}
+
+if (altura < 0 || altura > 3.00){
+    console.log("Altura inválida");
+}
+
+var pesoEhValido = true;
+var alturaEhValida = true;
