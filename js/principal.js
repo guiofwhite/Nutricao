15af623c86817a7b03987546fafca1689da047f3
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-28 10:01:44 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -50,5 +50,12 @@ function mostraMensagem(){
 var botaoAdicionar = document.querySelector("#adicionar-paciente");
 botaoAdicionar.addEventListener("click", function(event){
     event.preventDefault();
-    alert("Oi eu sou o botão e fui clicado");
+    //alert("Oi eu sou o botão e fui clicado");
 });
+
+var form = document.querySelector("#form-adiciona");
+
+var nome = form.nome.value;
+var peso = form.peso.value;
+var altura = form.altura.value;
+var gordura = form.gordura.value;
