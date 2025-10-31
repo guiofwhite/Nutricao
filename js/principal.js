b700e0411814e595594a419d9b9f6ef43ae40e59
Gui <marinho.silva.guilherme@escola.pr.gov.br>
2025-10-31 09:34:33 -0300

    a

--- js/principal.js
+++ js/principal.js
@@ -59,3 +59,11 @@ var nome = form.nome.value;
 var peso = form.peso.value;
 var altura = form.altura.value;
 var gordura = form.gordura.value;
+
+var pacienteTr = document.createElement("tr");
+
+var nomeTd = document.createElement("td");
+var pesoTd = document.createElement("td");
+var alturaTd = document.createElement("td");
+var gorduraTd = document.createElement("td");
+var imcTd = document.createElement("td");
